4c32760b2e6879ea4ec5710048dcdba36ba32fb2
Fuad2199 <fuad.beybutov099@gmail.com>
2025-06-20 01:19:10 +0400

    feat(subcategorymenu): improve submenu layout

--- components/modules/header/CategoryList.tsx
+++ components/modules/header/CategoryList.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const CategoryList = () => {
   return (
-    <div className="absolute h-[420px] w-60 shadow-xl top-[50px] border bg-white">
+    <div className="absolute h-109 w-60 shadow-xl top-16 border bg-white">
       <ul>
         <li className="h-9 w-full flex items-center justify-between px-4 group hover:bg-gray-50 cursor-pointer">
           <span className="text-body-sm-400 text-gray-600 capitalize group-hover:text-gray-900 cursor-pointer">
@@ -25,11 +25,69 @@ const CategoryList = () => {
             className="group-hover:text-gray-900"
           />
           {/* sub category menu */}
-          <div className="absolute h-full lg:w-175 xl:w-[868px] bg-white shadow-xl border top-0 left-62.5 p-3 grid gap-5 grid-cols-[164px_1fr_1fr]">
+          <div className="hidden group-hover:grid absolute h-full lg:w-175 xl:w-[868px] bg-white shadow-xl border top-0 left-62.5 p-5 gap-5 grid-cols-[100px_1fr_1fr] xl:grid-cols-[164px_1fr_1fr]">
             {/* column 1 */}
-            <div className="border"></div>
-            {/* column 2 */}
             <div className="border">
+              <ul>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    all
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    iPhone
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    samsung
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    realme
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    xiaomi
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    oppo
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    vivo
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    onePlus
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    huawei
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    infinix
+                  </span>
+                </li>
+                <li className="group hover:bg-gray-50 h-9 px-4 flex items-center">
+                  <span className="capitalize text-body-sm-400 text-gray-600 group-hover:text-body-sm-500">
+                    techno
+                  </span>
+                </li>
+              </ul>
+            </div>
+            {/* column 2 */}
+            <div className="">
               <div className="flex flex-col gap-4">
                 <span className="text-body-md-600 uppercase">
                   featured phones
@@ -88,7 +146,7 @@ const CategoryList = () => {
               </div>
             </div>
             {/* column 3 */}
-            <div className="border background-image-url"></div>
+            <div className="border background-image-url bg-center"></div>
           </div>
         </li>
         <li className="h-9 w-full flex items-center justify-between px-4 group hover:bg-gray-50 cursor-pointer">
@@ -123,6 +181,16 @@ const CategoryList = () => {
         <li className="h-9 w-full flex items-center justify-between px-4 group hover:bg-gray-50 cursor-pointer">
           <span className="text-body-sm-400 text-gray-600 capitalize group-hover:text-gray-900 group-hover:bg-gray-50">
             Watchs & Accessories
+          </span>
+        </li>
+        <li className="h-9 w-full flex items-center justify-between px-4 group hover:bg-gray-50 cursor-pointer">
+          <span className="text-body-sm-400 text-gray-600 capitalize group-hover:text-gray-900 group-hover:bg-gray-50">
+            GPS & navigation
+          </span>
+        </li>
+        <li className="h-9 w-full flex items-center justify-between px-4 group hover:bg-gray-50 cursor-pointer">
+          <span className="text-body-sm-400 text-gray-600 capitalize group-hover:text-gray-900 group-hover:bg-gray-50">
+            warable technology
           </span>
         </li>
       </ul>
